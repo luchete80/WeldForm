b4cd3734c5df2f6801a58f07a9cafe01c585b86c
lucho_inti <luciano.buglioni@gmail.com>
2020-02-14 14:15:54 -0300

    begining to add examples

---
+++ examples/auxs/matrices/matrices.cc
@@ -0,0 +1,19 @@
+#include <deal.II/numerics/matrix_tools.h>
+
+main()
+{
+	FullMatrix B(2,4);
+	
+	B(0,0)=
+	
+	//void 	mmult (FullMatrix< number2 > &C, const FullMatrix< number2 > &B, const bool adding=false) const
+	
+	// template void FullMatrix< number >::Tmmult< long double >	(	FullMatrix< number2 > & 	C,
+// const FullMatrix< number2 > & 	B,
+// const bool 	adding = false 
+// )		const
+
+	
+	mmult
+
+}
