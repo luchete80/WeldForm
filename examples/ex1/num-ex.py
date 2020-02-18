fe3385de06b90126a33b1dcb84693f83ec770ee6
luchete80 <33092006+luchete80@users.noreply.github.com>
2020-02-18 19:34:53 -0300

    Update num-ex.py

--- examples/ex1/num-ex.py
+++ examples/ex1/num-ex.py
@@ -7,7 +7,7 @@ init_printing(use_unicode=True)
 X2=Matrix(zeros(4,2)) 
 #X is combines per row to calculate J
 
-X2=[[]]
+X2=[[1,1],[0,1],[0,0],[1,0]]
 
 r, s = symbols('r s')
 h=[(1+r)*(1+s)/4,(1-r)*(1+s)/4,(1-r)*(1-s)/4,(1+r)*(1-s)/4]
@@ -17,9 +17,9 @@ J=Matrix(zeros(2,2))
 #Plane Strain 
 H=Matrix(zeros(2, 8))
 H=([  [h[0],0,h[1],0,h[2],0,h[3],0],[0,h[0],0,h[1],0,h[2],0,h[3]] ])
-dHrs=Matrix([ [diff(h[0],r),diff(h[1],r),diff(h[2],r),diff(h[3],r)], [diff(h[0],s),diff(h[1],s),diff(h[2],s),diff(h[3],s)] ])
+dHrs=Matrix([ [(1+s)/4,-(1+s)/4,-(1-s)/4,(1-s)/4], [(1+r)/4,(1-r)/4,-(1-r)/4,(1+r)/4] ])
 J=dHrs*X2
-#B=Matrix(J**(-1)*)
+B=Matrix(J**(-1)*)
 
 #dHdrs is [ [h1x,h2x,h3x,h4x],[h1y,h2y,h3y,h4y])
 #dHxy=J-1 dHrs
