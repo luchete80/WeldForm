3063aedba69115a5b0fe1be737ae05d6bf3f51a8
lucho-nb <luciano.buglioni@gmail.com>
2021-02-25 17:19:15 -0300

    Working on #1

--- Source/Domain.cpp
+++ Source/Domain.cpp
@@ -1319,24 +1319,57 @@ inline void Domain::Move (double dt)
 }
 
 inline void Domain::CalcTempInc () {
-	#pragma omp parallel for schedule (static) num_threads(Nproc)
-	for (size_t k=0; k<Nproc;k++) {
+	double di=0.0,dj=0.0,mi=0.0,mj=0.0;
+			// if (!P1->IsFree)
+			// {
+				// di = DensitySolid(P2->PresEq, P2->Cs, P2->P0,P1->Pressure, P2->RefDensity);
+				// mi = P1->FPMassC * P2->Mass;
+			// }
+			// else
+			// {
+				// di = P1->Density;
+				// mi = P1->Mass;
+			// }
+			// if (!P2->IsFree)
+			// {
+				// dj = DensitySolid(P1->PresEq, P1->Cs, P1->P0,P2->Pressure, P1->RefDensity);
+				// mj = P2->FPMassC * P1->Mass;
+			// }
+			// else
+			// {
+				// dj = P2->Density;
+				// mj = P2->Mass;
+			// }
+	//#pragma omp parallel for schedule (static) num_threads(Nproc)
+	for ( size_t k = 0; k < Nproc ; k++) {
 		size_t P1,P2;
 		Vec3_t xij;
-		double h,K;
+		double h,GK;
 		//TODO: DO THE LOCK PARALLEL THING
 		// Summing the smoothed pressure, velocity and stress for fixed particles from neighbour particles
-		for ( size_t a=0; a<FSMPairs[k].Size();a++ ) {
-			P1	= FSMPairs[k][a].first;
-			P2	= FSMPairs[k][a].second;
+		double temp;
+		for (size_t i=0; i<SMPairs[k].Size();i++) {
+			P1	= SMPairs[k][a].first;
+			P2	= SMPairs[k][a].second;
 			xij	= Particles[P1]->x-Particles[P2]->x;
 			h	= (Particles[P1]->h+Particles[P2]->h)/2.0;
-
-			Periodic_X_Correction(xij, h, Particles[P1], Particles[P2]);
-
-			K	= Kernel(Dimension, KernelType, norm(xij)/h, h);
+			GK	= GradKernel(Dimension, KernelType, norm(xij)/h, h);
+			di = P1->Density; mi = P1->Mass;
+			dj = P2->Density; mj = P2->Mass;
+			//Frasier  Eqn 3.99 dTi/dt= 1/(rhoi_CPi) * Sum_j(mj/rho_j * 4*ki kj/ (ki + kj ) (Ti - Tj)  ) 
+			temp += mj/dj * 4. * ( P1->k_T * P2->k_T) / (P1->k_T + P2->k_T) * ( P1->T * P2->T) * dot( xij , GK*xij );
+			omp_set_lock(&P1->my_lock);
+				P1->a		+= temp;
+			omp_unset_lock(&P1->my_lock);
+			// Locking the particle 2 for updating the properties
+			omp_set_lock(&P2->my_lock);
+				P2->a		-= mi * temp;
+			omp_unset_lock(&P2->my_lock);
 		}
-	}
+		// for (size_t i=0; i<FSMPairs[k].Size();i++)
+			// CalcForce2233(Particles[FSMPairs[k][i].first],Particles[FSMPairs[k][i].second]);
+	}//Nproc
+
 }
 
 inline void Domain::InFlowBCLeave()
