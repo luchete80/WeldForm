a8649b393ed9a5ed6072890ffc99d5ba7af1c7be
luchonbdell} <luciano.buglioni@gmail.com>
2021-05-04 16:58:24 -0300

    Working on #55

--- Source/Domain.h
+++ Source/Domain.h
@@ -64,6 +64,11 @@ public:
     void AddSingleParticle	(int tag, Vec3_t const & x, double Mass, double Density, double h, bool Fixed);		//Add one particle
     void AddBoxLength				(int tag, Vec3_t const &V, double Lx, double Ly, double Lz,double r, double Density,
 																	double h,int type, int rotation, bool random, bool Fixed);									//Add a cube of particles with a defined dimensions
+
+    void AddCylinderLength			(int tag, Vec3_t const &V, double Rxy, double Lz,double r, double Density,
+																	double h,int type, int rotation, bool random, bool Fixed);									//Add a cube of particles with a defined dimensions
+
+
     void AddBoxNo						(int tag, Vec3_t const &V, size_t nx, size_t ny, size_t nz,double r, double Density,
 																	double h,int type, int rotation, bool random, bool Fixed);									//Add a cube of particles with a defined numbers
     void DelParticles				(int const & Tags);					//Delete particles by tag
