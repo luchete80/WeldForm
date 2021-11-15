7edb2bda0fb9c9417b6e25e8b4a498a324b0dcd6
luciano INTI Win <luciano.buglioni@gmail.com>
2021-11-15 13:36:55 -0300

    working on meshç

--- Source/Mesh.cpp
+++ Source/Mesh.cpp
@@ -11,7 +11,7 @@ Mesh::AxisPlaneMesh(const int &axis, const int &offset, const Vec3_t p1, const V
 	
 	double x,y,z;
 	if (axis == 0 )
-	{x = p1(0);}
+	{x = p1(0); y= }
 	
 	for (int i=0; i<dens+1; i++) {
 		for (int j=0; j<dens+1; j++){
