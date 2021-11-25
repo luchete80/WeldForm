b8cc32320f9a29f656a342a09dafae9a11386ed8
pcfiuba2 <luciano.buglioni@gmail.com>
2021-11-25 14:25:01 -0300

    adding contact detection

--- Source/Contact.cpp
+++ Source/Contact.cpp
@@ -82,12 +82,20 @@ void Domain::DetectContactPoints(){
 	for (int k=0; k<Nproc;k++) 
 	#endif	
 	{
-		size_t P1,P2;
+		Particle* P1,P2;
 		Vec3_t xij;
 		double h,K;
 		// Summing the smoothed pressure, velocity and stress for fixed particles from neighbour particles
 		//IT IS CONVENIENT TO FIX SINCE FSMPairs are significantly smaller
-		for (size_t a=0; a<FSMPairs[k].Size();a++) {
+		for (size_t a = 0; a < ContPairs[k].Size();a++) {
+			P1	= Particles [ContPairs[k][a].first];
+			P2	= Particles [ContPairs[k][a].second];				
+			Particle* pi, pj; //SPH and contact part
+			if (P1->ID == contact_surf_id ) { pi = P2; pj = P1; }
+			else														{ pi = P1; pj = P2; }
+		
+			Vec3_t vr = pi->v - pj->v;		//Fraser 3-137
+			double delta = - dot( , vr);	//Penetration rate, Fraser 3-138
 		}
 	}//Nproc
 }
