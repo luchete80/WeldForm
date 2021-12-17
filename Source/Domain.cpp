f58cec5577cacdc0e8b9bbe97f5b23d8fb7c4b1c
pcfiuba2 <luciano.buglioni@gmail.com>
2021-12-17 20:29:36 -0300

    saving contact

--- Source/Domain.cpp
+++ Source/Domain.cpp
@@ -1206,7 +1206,7 @@ inline void Domain::Solve (double tf, double dt, double dtOut, char const * TheF
 				SaveNeighbourData();				//Necesary to calulate surface! Using Particle->Nb (count), could be included in search
 				CalculateSurface(1);				//After Nb search			
 				ContactNbSearch();
-				SaveNeighbourData();	//Again Save Nb data
+				//SaveNeighbourData();	//Again Save Nb data
 			}//contact
 			isyielding  = true ;
 		}
@@ -1238,6 +1238,7 @@ inline void Domain::Solve (double tf, double dt, double dtOut, char const * TheF
 					SaveNeighbourData();				//Necesary to calulate surface! Using Particle->Nb (count), could be included in search
 					CalculateSurface(1);				//After Nb search			
 					ContactNbSearch();
+					SaveContNeighbourData();
 					//SaveNeighbourData();	//Again Save Nb data
 				}//contact				
 				contact_time_spent += (double)(clock() - clock_beg) / CLOCKS_PER_SEC;
