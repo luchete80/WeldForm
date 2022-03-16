93c27085e479e83fac92fbf00af22e2b23066a81
pcfiuba2 <luciano.buglioni@gmail.com>
2022-03-16 10:19:28 -0300

    Changing some examples

--- Traction.cpp
+++ Traction.cpp
@@ -131,7 +131,7 @@ int main(int argc, char **argv) try
 
 		cout << "Particle count: "<<dom.Particles.Size()<<endl;
 		
-		dom.Particles[6106]->print_history = true;
+		dom.Particles[6777]->print_history = true;
 		
     	for (size_t a=0; a<dom.Particles.Size(); a++)
     	{
