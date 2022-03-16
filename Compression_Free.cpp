47923482c6eee6e2e91aba4183a8647ad4938d0f
pcfiuba2 <luciano.buglioni@gmail.com>
2022-03-16 08:30:00 -0300

    removed log things, and made parallel again

--- Compression_Free.cpp
+++ Compression_Free.cpp
@@ -90,12 +90,13 @@ int main(int argc, char **argv) try
 		Fy	= 300.e6;
     	//dx	= L / (n-1);
 		//dx = L/(n-1);
-		dx = 0.030;
+		dx = 0.010;
     h	= dx*1.2; //Very important
         Cs	= sqrt(K/rho);
 
         double timestep;
         timestep = (0.2*h/(Cs));
+				timestep = 1.0e-7;
 		
 		//timestep = 2.5e-6;
 
@@ -146,7 +147,7 @@ int main(int argc, char **argv) try
 
     //dom.Solve_orig_Ext(/*tf*/0.00205,/*dt*/timestep,/*dtOut*/0.001,"test06",999);
 		//dom.Solve(/*tf*//*0.0105*/4.*timestep+1e-8,/*dt*/timestep,/*dtOut*/timestep,"test_gpu",999);
-		dom.Solve(0.001,/*dt*/timestep,/*dtOut*/0.0001,"test06",999);
+		dom.Solve(0.00101,/*dt*/timestep,/*dtOut*/0.0001,"test06",999);
     
 		return 0;
 }
