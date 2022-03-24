12b9823eb9607aaf5f91373f95ccd218b9d318fb
luciano nb <luciano.buglioni@gmail.com>
2022-03-23 22:51:49 -0300

    added mixed gradient correction

--- Source/Domain.cpp
+++ Source/Domain.cpp
@@ -1159,6 +1159,107 @@ inline void Domain::CalcGradCorrMatrix () {
 	}	
 }
 
+//New, for Bonet gradient correction
+inline void Domain::CalcGradCorrMixedMatrix () {
+	double di=0.0,dj=0.0,mi=0.0,mj=0.0;
+	
+	std::vector < Mat3_t> temp(Particles.Size());
+	Mat3_t m,mt[2];
+	
+	//cout << "Applying grad corr"<<endl;
+	//#pragma omp parallel for schedule (static) num_threads(Nproc) //LUCIANO: THIS IS DONE SAME AS PrimaryComputeAcceleration
+  double sumden[Particles.Size()];
+  Vec3_t gamma [Particles.Size()];
+  
+	for ( size_t k = 0; k < Nproc ; k++) {
+		Particle *P1,*P2;
+		Vec3_t xij;
+		double h,GK,K;
+    int i,j;
+		//cout << "SMPairs[k].Size()"<<SMPairs[k].Size()<<endl;
+		//TODO: DO THE LOCK PARALLEL THING
+		for (size_t a=0; a<SMPairs[k].Size();a++) {//Same Material Pairs, Similar to Domain::LastComputeAcceleration ()
+			//cout << "a: " << a << "p1: " << SMPairs[k][a].first << ", p2: "<< SMPairs[k][a].second<<endl;
+      
+			P1	= Particles[SMPairs[k][a].first];
+			P2	= Particles[SMPairs[k][a].second];
+			xij	= P1->x - P2->x;
+			h	= (P1->h+P2->h)/2.0;
+			GK	= GradKernel(Dimension, KernelType, norm(xij)/h, h);				
+      K	  =     Kernel(Dimension, KernelType, norm(xij)/h, h);
+      
+      sumden [i]+= mj/dj * K;
+      sumden [j]-= mi/di * K;
+      //Bonet Eqn 57  
+      gamma [i] += GK*xij*mj/dj;
+      gamma [j] -= GK*xij*mi/di;      
+			
+			di = P1->Density; mi = P1->Mass;
+			dj = P2->Density; mj = P2->Mass;
+		
+			Dyad (Vec3_t(GK*xij),xij,m);
+			mt[0] = mj/dj * m;
+			mt[1] = mi/di * m;
+			//cout << "mt"<<mt[0]<<endl;
+			//omp_set_lock(&P1->my_lock);
+			//SIGN IS NEGATIVE (IF POSITIVE, GRADIENT SIGN IS OPPOSITE)
+			
+			temp[SMPairs[k][a].first]  = temp[SMPairs[k][a].first]  - mt[0];  
+			temp[SMPairs[k][a].second] = temp[SMPairs[k][a].second] - mt[1];
+		}
+	}//Nproc
+	//cout << "Fixed Pairs"<<endl;
+	for ( size_t k = 0; k < Nproc ; k++) {
+		Particle *P1,*P2;
+		Vec3_t xij;
+		double h,GK;
+		//TODO: DO THE LOCK PARALLEL THING
+		//cout << "FSMPairs[k].Size()"<<FSMPairs[k].Size()<<endl;
+		for (size_t a=0; a<FSMPairs[k].Size();a++) {//Same Material Pairs, Similar to Domain::LastComputeAcceleration ()
+			//cout << "a: " << a << "p1: " << SMPairs[k][a].first << ", p2: "<< SMPairs[k][a].second<<endl;
+			P1	= Particles[FSMPairs[k][a].first];
+			P2	= Particles[FSMPairs[k][a].second];
+			xij	= P1->x - P2->x;
+			h	= (P1->h+P2->h)/2.0;
+			GK	= GradKernel(Dimension, KernelType, norm(xij)/h, h);	
+			
+			di = P1->Density; mi = P1->Mass;
+			dj = P2->Density; mj = P2->Mass;
+		
+			Dyad (Vec3_t(GK*xij),xij,m);
+			mt[0] = mj/dj * m;
+			mt[1] = mi/di * m;
+			//omp_set_lock(&P1->my_lock);
+			//SIGN IS NEGATIVE (IF POSITIVE, GRADIENT SIGN IS OPPOSITE)
+			
+			temp[FSMPairs[k][a].first]  = temp[FSMPairs[k][a].first]  - mt[0];  
+			temp[FSMPairs[k][a].second] = temp[FSMPairs[k][a].second] - mt[1];
+		}
+	}//Nproc
+	//cout << "Inverting"<<endl;
+	//#pragma omp parallel for schedule (static) num_threads(Nproc)	//LUCIANO//LIKE IN DOMAIN->MOVE
+	//cout << "Inverting"<<endl;
+	for (int i=0; i<Particles.Size(); i++){
+		// cout << "part "<<i<<endl;
+		//cout << "x: "<<Particles[i]->x<<endl;
+		// cout << "nb: "<<Particles[i]->Nb<<endl;
+		// if (!Particles[i]->IsFree) cout << "Fixed"<<endl;
+		//cout << "temp "<<temp[i]<<endl;
+		if (Dimension == 2)
+			temp[i](2,2) = 1;
+		/** Inverse.*/
+		//inline void Inv (Mat3_t const & M, Mat3_t & Mi, double Tol=1.0e-10)}	
+		if (Particles[i]->IsFree){
+			Inv(temp[i],m);	
+
+			Particles[i] ->gradCorrM = m;
+			//cout << "Corr Matrix: " << m <<endl;
+		} else {
+			Particles[i] ->gradCorrM = I;
+		}
+	}	
+}
+
 inline void Domain::Move (double dt) {
 	//cout << "BEGIN MOVE Time " << Time << "------------------------------"<<endl;
 	#pragma omp parallel for schedule (static) num_threads(Nproc)
