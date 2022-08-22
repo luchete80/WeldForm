7190e26237202d25631873a1eb253b4152731202
pcfiuba2 <luciano.buglioni@gmail.com>
2022-08-22 15:33:49 -0300

    Working on plastic and friction work

--- Compression_thermal.cpp
+++ Compression_thermal.cpp
@@ -92,7 +92,7 @@ int main(int argc, char **argv) try
     	//dx	= L / (n-1);
 		//dx = L/(n-1);
 		dx = 0.015;
-    h	= dx*1.1; //Very important
+    h	= dx*1.2; //Very important
         Cs	= sqrt(K/rho);
 
         double timestep;
