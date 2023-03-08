f31d41021fe6f1ff31ee9e3c4ff68b4f204134f0
luchete80 <luciano.buglioni@gmail.com>
2023-03-08 10:48:14 -0300

    updated fsw example

--- examples/FSW_red.cpp
+++ examples/FSW_red.cpp
@@ -187,8 +187,8 @@ int main(int argc, char **argv) try
       //dom.Particles[a]->Sigmay	= Fy;
       dom.Particles[a]->Sigmay	= mat.CalcYieldStress(0.0,0.0,0.);    
 			
-      dom.Particles[a]->Alpha		= 1.0;
-			//dom.Particles[a]->Beta		= 2.0;
+      dom.Particles[a]->Alpha		= 2.5;
+			dom.Particles[a]->Beta		= 2.5;
 			dom.Particles[a]->TI		= 0.3;
 			dom.Particles[a]->TIInitDist	= dx;
       
