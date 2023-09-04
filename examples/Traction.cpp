802b47bbc7aad74a139955444131f021af279672
luciano_inti-mswin <luciano.buglioni@gmail.com>
2023-09-04 10:18:54 -0300

    Fraser solver also works on traction example

--- examples/Traction.cpp
+++ examples/Traction.cpp
@@ -20,6 +20,7 @@
 
 #include "Domain.h"
 #include "InteractionAlt.cpp"
+#include "SolverFraser.cpp"
 #include "SolverLeapfrog.cpp"
 
 #define TAU		0.005
@@ -178,9 +179,9 @@ int main(int argc, char **argv) try
    //dom.Solve(/*tf*/0.0505,/*dt*/timestep,/*dtOut*/0.0001,"test06",999);
     timestep = (0.4*h/(Cs)); //Standard modified Verlet do not accept such step
     dom.auto_ts=false; 
-    //dom.Solve(/*tf*/0.0105,/*dt*/timestep,/*dtOut*/0.0001,"test06",999);
-    //dom.SolveDiffUpdateKickDrift(/*tf*/0.0105,/*dt*/timestep,/*dtOut*/1.e-4 ,"test06",1000);        
+    
     dom.SolveDiffUpdateLeapFrog(/*tf*/0.0105,/*dt*/timestep,/*dtOut*/1.e-4 ,"test06",1000);                
+    //dom.SolveDiffUpdateFraser(/*tf*/0.0105,/*dt*/timestep,/*dtOut*/1.e-4 ,"test06",1000);      
         return 0;
 }
 
