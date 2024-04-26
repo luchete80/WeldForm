ffe0d3c8dc4e7c5026cc43836b0936ca8fd0a1a0
luciano nb <luciano.buglioni@gmail.com>
2024-04-26 17:41:26 -0300

    added things

--- Source/InteractionAlt.cpp
+++ Source/InteractionAlt.cpp
@@ -147,11 +147,11 @@ inline void Domain::CalcAccel() {
           Vec3_t wij = GK*xij;
           // di = P1->etaDens;
           // dj = P2->etaDens;
-          temp[0] =( (Sigmai(0,0)*P1->x(0)*1./(di*di) + Sigmaj(0,0) *P2->x(0)* 1./(dj*dj)) *wij(0) + 
-                    (Sigmai(0,1)*P1->x(0)*1./(di*di) + Sigmaj(0,1) *P2->x(0)* 1./(dj*dj)) *wij(1) )* 2.0 * M_PI;  ////dvr/dt 2PI can go in the reduction
+          temp[0] = (Sigmai(0,0)*P1->x(0)*1./(di*di) + Sigmaj(0,0) *P2->x(0)* 1./(dj*dj)) *wij(0) + 
+                    (Sigmai(0,1)*P1->x(0)*1./(di*di) + Sigmaj(0,1) *P2->x(0)* 1./(dj*dj)) *wij(1) ;  ////dvr/dt 2PI can go in the reduction
 
-          temp[1] =( (Sigmai(0,1)*P1->x(0)*1./(di*di) + Sigmaj(0,1) *P2->x(0)* 1./(dj*dj)) *wij(0) + 
-                    (Sigmai(1,1)*P1->x(0)*1./(di*di) + Sigmaj(1,1) *P2->x(0)* 1./(dj*dj)) *wij(1) )*2.0 * M_PI;  ////dvr/dt 2PI can go in the reduction
+          temp[1] = (Sigmai(0,1)*P1->x(0)*1./(di*di) + Sigmaj(0,1) *P2->x(0)* 1./(dj*dj)) *wij(0) + 
+                    (Sigmai(1,1)*P1->x(0)*1./(di*di) + Sigmaj(1,1) *P2->x(0)* 1./(dj*dj)) *wij(1) ;  ////dvr/dt 2PI can go in the reduction
           
           
         //}
@@ -244,7 +244,7 @@ inline void Domain::AccelReduction(){
       //ADD HOOP ACCEL AND MULT BY 2PI
       #pragma omp parallel for schedule (static) num_threads(Nproc)
       for (int i=0; i<solid_part_count;i++){
-        //Particles[i]->a *= 2.0 * M_PI; //PREVIOUSLY
+        Particles[i]->a *= 2.0 * M_PI; //PREVIOUSLY
         Particles[i]->a[0] -= 2.0 * M_PI * Particles[i]->Sigma(2,2); //WANG Eqn. 40
         Particles[i]->a[2] = - 2.0*M_PI*Particles[i]->Sigma(2,2)/Particles[i]->Density; //DIRECT HOOP EQN 44
       }
